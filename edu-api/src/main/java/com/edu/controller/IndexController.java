c215421dcec12b959a87730ea456528717e45d02
wxueyanghj@163.com <wxueyanghj@163.com>
2019-08-14 20:12:34 +0800

    login页面提交

--- edu-api/src/main/java/com/edu/controller/IndexController.java
+++ edu-api/src/main/java/com/edu/controller/IndexController.java
@@ -4,14 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @RequestMapping
 @Controller
 @Slf4j
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Map<String, String> map){
         log.info("开始进入登录页面请求");
+        map.put("name","wangxueyang");
+        map.put("age","18");
         return "login";
     }
 }
